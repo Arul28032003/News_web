8d26538a242ad7308c65be71865f1a346aaa99cd
Arul28032003 <128033440+Arul28032003@users.noreply.github.com>
2024-03-10 17:40:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def display_news(news):
 #Data fetch from data base using key
 DETA_KEY = st.secrets["data_key"]
 deta = Deta(DETA_KEY)
-db = deta.Base("data")
+db = deta.Base("Workshop")
 #Header area
 st.caption("👆👈 Click the arrow at the top-left corner to select a category.")
 st.title("⇝ TrendSpoter 📰✉️🚀")
